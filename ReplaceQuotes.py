14ea238b376546a22e6314f0eeb4b06fbdff5bb9
Christopher Strack <christopher.strack@gmail.com>
2014-03-04 21:53:03 +0100

    Create base class for replace command

--- ReplaceQuotes.py
+++ ReplaceQuotes.py
@@ -5,34 +5,41 @@ from sublime_plugin import TextCommand
 from sublime import Region
 
 
-_pattern_double = re.compile(r'"(.+?)"', re.DOTALL)
-_pattern_single = re.compile(r'\'(.+?)\'', re.DOTALL)
+class ReplaceCommand(TextCommand):
 
-def replace_quotes(text, quote_begin, quote_end):
-    s = _pattern_double.sub(r'%s\1%s' % (quote_begin, quote_end), text)
-    return _pattern_single.sub(r'‘\1’', s)
+    def replace(self, text):
+        raise NotImplementedError
 
+    def run(self, edit):
+        if not self.replace_selection(edit):
+            self.replace_all(edit)
 
-class ReplaceQuotesCommand(TextCommand):
-
-    def run(self, edit, quote_begin, quote_end):
-        if not self.replace_selection(edit, quote_begin, quote_end):
-            self.replace_all(edit, quote_begin, quote_end)
-
-    def replace_selection(self, edit, quote_begin, quote_end):
+    def replace_selection(self, edit):
         replaced = False
         for region in filter(lambda r: not r.empty(), self.view.sel()):
-            self.replace_region(
-                edit, region,
-                quote_begin, quote_end)
+            self.replace_region(edit, region)
             replaced = True
         return replaced
 
-    def replace_all(self, edit, quote_begin, quote_end):
-        self.replace_region(
-            edit, Region(0, self.view.size()),
-            quote_begin, quote_end)
+    def replace_all(self, edit):
+        self.replace_region(edit, Region(0, self.view.size()))
 
-    def replace_region(self, edit, region, *a, **k):
-        s = replace_quotes(self.view.substr(region), *a, **k)
+    def replace_region(self, edit, region):
+        s = self.replace(self.view.substr(region))
         self.view.replace(edit, region, s)
+
+
+class ReplaceQuotesCommand(ReplaceCommand):
+
+    _pattern_double = re.compile(r'"(.+?)"', re.DOTALL)
+    _pattern_single = re.compile(r'\'(.+?)\'', re.DOTALL)
+
+    def run(self, edit, quote_begin, quote_end):
+        self._quote_begin = quote_begin
+        self._quote_end = quote_end
+        super().run(edit)
+
+    def replace(self, text):
+        s = self._pattern_double.sub(
+            r'%s\1%s' % (self._quote_begin, self._quote_end), text)
+        return self._pattern_single.sub(r'‘\1’', s)
